ce39e06fd1cb0bd9afc4ccbb21e9a012771596d3
Kamil Poznakowski <kamil.poznakowski@kaseka.lan>
2016-09-30 16:55:13 +0200

    2 wskazowki, listener wstepnie dziala na jednej

--- app/src/main/java/com/kaseka/boxmaptest1/AlarmClockActivity.java
+++ app/src/main/java/com/kaseka/boxmaptest1/AlarmClockActivity.java
@@ -27,6 +27,7 @@ public class AlarmClockActivity extends AppCompatActivity {
     private String format = "";
     //Resources system;
     private ImageView ivHourHand;
+    private ImageView ivMinuteHand;
     private RelativeLayout rlClockParent;
     private View vClockCenterCircle;
     private Point centerPt, targetPt;
@@ -38,6 +39,9 @@ public class AlarmClockActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm_clock);
 
         ivHourHand = (ImageView) findViewById(R.id.ivHourHand);
+        ivHourHand.setAlpha(0.5f);
+        ivMinuteHand = (ImageView) findViewById(R.id.ivMinuteHand);
+        ivHourHand.setAlpha(0.5f);
         rlClockParent = (RelativeLayout) findViewById(R.id.rlClockParent);
         vClockCenterCircle = findViewById(R.id.vClockCenterCircle);
 
@@ -54,54 +58,23 @@ public class AlarmClockActivity extends AppCompatActivity {
                 Log.d("params.leftMargin", String.valueOf(params.leftMargin));
                 params.topMargin = (int)vClockCenterCircle.getTop() + (vClockCenterCircle.getLayoutParams().height / 2) - (ivHourHand.getHeight()/2);
                 ivHourHand.setLayoutParams(params);
+                ivMinuteHand.setLayoutParams(params);
 
                 ivHourHand.setPivotX(0);
+                ivMinuteHand.setPivotX(0);
                 ivHourHand.setPivotY(ivHourHand.getHeight()/2);
+                ivMinuteHand.setPivotY(ivHourHand.getHeight()/2);
                 //ivHourHand.setRotation(30);
             }
         });
 
-//        rlClockParent.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-////                final GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
-////                    public void onLongPress(MotionEvent e) {
-////                        Log.e("", "Longpress detected");
-////                    }
-////                });
-//                int middleX = (int)vClockCenterCircle.getLeft() + (vClockCenterCircle.getLayoutParams().width / 2);
-//                int middleY = (int)vClockCenterCircle.getTop() + (vClockCenterCircle.getLayoutParams().height / 2) - (ivHourHand.getHeight()/2);
-//                Log.d("Point middleX", String.valueOf(middleX));
-//                Log.d("Point middleY", String.valueOf(middleY));
-//                int eventX = (int)event.getX();
-//                int eventY = (int)event.getY();
-//                Log.d("Point touchX", String.valueOf(eventX));
-//                Log.d("Point touchY", String.valueOf(eventY));
-//
-//                centerPt = new Point(middleX, middleY);
-//                targetPt = new Point(eventX, eventY);
-//                float angle = (float)AlarmClockActivity.calcRotationAngleInDegrees(centerPt,targetPt);
-//                ivHourHand.setRotation(angle);
-//
-//
-//
-//             //return true;
-//             return true;
-//            }
-//        });
-
-        ivHourHand.setOnLongClickListener(new View.OnLongClickListener() {
+        ivHourHand.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                ivHourHand.setAlpha(0.5f);
+            public void onClick(View v) {
+                ivHourHand.setAlpha(1.0f);
                 rlClockParent.setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
-//                final GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
-//                    public void onLongPress(MotionEvent e) {
-//                        Log.e("", "Longpress detected");
-//                    }
-//                });
                         int middleX = (int)vClockCenterCircle.getLeft() + (vClockCenterCircle.getLayoutParams().width / 2);
                         int middleY = (int)vClockCenterCircle.getTop() + (vClockCenterCircle.getLayoutParams().height / 2) - (ivHourHand.getHeight()/2);
                         Log.d("Point middleX", String.valueOf(middleX));
@@ -115,19 +88,11 @@ public class AlarmClockActivity extends AppCompatActivity {
                         targetPt = new Point(eventX, eventY);
                         float angle = (float)AlarmClockActivity.calcRotationAngleInDegrees(centerPt,targetPt);
                         ivHourHand.setRotation(angle);
-
-
-
-                        //return true;
                         return true;
                     }
                 });
-
-                isMinuteHand = false;
-                return false;
             }
         });
-
     }
 
     public static double calcRotationAngleInDegrees(Point centerPt, Point targetPt)

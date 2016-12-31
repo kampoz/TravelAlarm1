d5a70dde1bd3b398198825242cec8a336d06d1f0
Kamil Poznakowski <kamil.poznakowski@kaseka.lan>
2016-12-31 15:26:50 +0100

    cd

--- app/src/main/java/com/kaseka/boxmaptest1/dialog/AlarmOnOffFragment.java
+++ app/src/main/java/com/kaseka/boxmaptest1/dialog/AlarmOnOffFragment.java
@@ -39,9 +39,6 @@ public class AlarmOnOffFragment extends DialogFragment{
         final TextView tvDialogTravelTime = (TextView) view.findViewById(R.id.tvDialogTravelTime);
         final ImageButton bDeleteAlarm = (ImageButton) view.findViewById(R.id.ibDeleteAlarm);
 
-
-
-
         //tvAlarmHour.setText(AlarmPOJO.getAlarmHour()+" : "+AlarmPOJO.getAlarmMinute());
         tvAlarmHour.setText(MyDisplayTimeHelper.setDisplayTime(String.valueOf(AlarmPOJO.getAlarmHour()),String.valueOf(AlarmPOJO.getAlarmMinute())));
         tvDialogWeekDay.setText("Alarm day: "+AlarmPOJO.getAlarmDayOfWeek());
@@ -55,53 +52,9 @@ public class AlarmOnOffFragment extends DialogFragment{
         tvDialogTravelTime.setText("travel time: "+AlarmPOJO.getRouteTimeLabel());
         isOn = AlarmPOJO.getIsOn();
 
-//        String onOffButtonLabel = "";
-//        if(isOn){
-//            onOffButtonLabel = "OFF";
-//        }else onOffButtonLabel = "ON";
-//
         builder.setView(view);
-//        builder.setNegativeButton(onOffButtonLabel, new DialogInterface.OnClickListener() {
-//
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                if(isOn){
-//                    isOn=false;
-//                    AlarmPOJO.setIsOn(false);
-//                    AlarmPOJO.setIsOntoRealm();
-//
-//                }else {
-//                    isOn=true;
-//                    AlarmPOJO.setIsOn(false);
-//                AlarmPOJO.setIsOntoRealm();
-//                }
-
-//            }
-//        });
-
-//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                AlarmPOJO.insertAlarmToRealm();
-//                Intent startAlarmListActivityIntent = new Intent(getActivity(), AlarmsListActivity.class);
-//                getActivity().startActivity(startAlarmListActivityIntent);
-//            }
-//        });
-
-//        bDeleteAlarm.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(v.getContext(), "CloseAlarm ib", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
         Dialog dialog = builder.create();
         return dialog;
-
-
-
     }
-
 
 }

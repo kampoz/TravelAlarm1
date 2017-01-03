a09b094aabe56f70ec92c3d54875862fb872d706
Kamil Poznakowski <kamil.poznakowski@kaseka.lan>
2017-01-03 12:53:42 +0100

    cd poprawka styles.xml

--- app/src/main/java/com/kaseka/boxmaptest1/activity/AlarmsListActivity.java
+++ app/src/main/java/com/kaseka/boxmaptest1/activity/AlarmsListActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -34,6 +35,8 @@ public class AlarmsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarms_list);
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
         //getSupportActionBar().hide();
 
         bAddAlarm = (Button)findViewById(R.id.bAddAlarm);

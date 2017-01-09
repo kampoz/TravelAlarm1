05ec728de7003b346f975ce9cbad0a28900d4987
Kamil Poznakowski <kamil.poznakowski@kaseka.lan>
2017-01-09 17:41:30 +0100

    UI activity SettingsActivity.java

--- app/src/main/java/com/kaseka/boxmaptest1/activity/SettingsActivity.java
+++ app/src/main/java/com/kaseka/boxmaptest1/activity/SettingsActivity.java
@@ -2,17 +2,33 @@ package com.kaseka.boxmaptest1.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
 
 import com.kaseka.boxmaptest1.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private RadioButton rbSound1,rbSound2,rbSound3;
+    private ImageButton ibPlaySound1, ibPlaySound2, ibPlaySound3;
+    private Button bSetAlarmSound;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private boolean playSound1 = false;
+    private boolean playSound2 = false;
+    private boolean playSound3 = false;
+    private boolean playSound = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +38,106 @@ public class SettingsActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         toolbar.setTitle(R.string.title_activity_settings);
         setSupportActionBar(toolbar);
+
+        rbSound1 = (RadioButton)findViewById(R.id.rbSound1);
+        rbSound2 = (RadioButton)findViewById(R.id.rbSound2);
+        rbSound3 = (RadioButton)findViewById(R.id.rbSound3);
+
+        ibPlaySound1 = (ImageButton) findViewById(R.id.ibPlaySound1);
+        ibPlaySound2 = (ImageButton) findViewById(R.id.ibPlaySound2);
+        ibPlaySound3 = (ImageButton) findViewById(R.id.ibPlaySound3);
+
+        rbSound1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbSound1.setChecked(true);
+                rbSound2.setChecked(false);
+                rbSound3.setChecked(false);
+            }
+        });
+
+        rbSound2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbSound1.setChecked(false);
+                rbSound2.setChecked(true);
+                rbSound3.setChecked(false);
+            }
+        });
+
+        rbSound3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbSound1.setChecked(false);
+                rbSound2.setChecked(false);
+                rbSound3.setChecked(true);
+            }
+        });
+
+        ibPlaySound1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!playSound1){
+                    playSound1 = true;
+                    soundButtonsReaction(ibPlaySound1);
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(SettingsActivity.this, R.raw.sound1);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.start();
+
+                }else{
+                    mediaPlayer.stop();
+                    playSound1 = false;
+                    setColorFiltersToNull();
+                }
+            }
+        });
+
+        ibPlaySound2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!playSound2){
+                    playSound2 = true;
+                    soundButtonsReaction(ibPlaySound2);
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(SettingsActivity.this, R.raw.sound2);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.start();
+
+                }else{
+                    mediaPlayer.stop();
+                    playSound2 = false;
+                    setColorFiltersToNull();
+                }
+            }
+        });
+
+        ibPlaySound3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!playSound3){
+                    playSound3 = true;
+                    soundButtonsReaction(ibPlaySound3);
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(SettingsActivity.this, R.raw.sound3);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.start();
+
+                }else{
+                    mediaPlayer.stop();
+                    playSound3 = false;
+                    setColorFiltersToNull();
+                }
+
+            }
+        });
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,5 +177,19 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void soundButtonsReaction(ImageButton imageButton) {
+            ibPlaySound1.setColorFilter(null);
+            ibPlaySound2.setColorFilter(null);
+            ibPlaySound3.setColorFilter(null);
+            imageButton.setColorFilter(getResources().getColor(R.color.colorMyLightGreen));
+        }
+
+    private void setColorFiltersToNull(){
+        ibPlaySound1.setColorFilter(null);
+        ibPlaySound2.setColorFilter(null);
+        ibPlaySound3.setColorFilter(null);
+
     }
 }
